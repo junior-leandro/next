7900231a4d0e0d034627784c4876a32e57be246a
Junior Oliveira Leandro <contato_jr@outlook.com>
2023-07-21 12:58:58 -0300

    atualização do commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export default function HomePage(){
+    return (
+        <div className="text-center">
+            <span className="italic">Página Inicial do projeto em andamento...</span>
+        </div>
+    )
+}
